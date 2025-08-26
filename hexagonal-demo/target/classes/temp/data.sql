c6bf825f9e8e4409089bd6d7cc3928e4682c2186
Monise Jacheta <monisel.jacheta@gmail.com>
2025-08-26 14:03:04 -0300

    Delete hexagonal-demo directory

--- hexagonal-demo/target/classes/temp/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO conta_corrente (nome_do_titular, numero_da_conta, saldo_atual)
-VALUES ('João Silva', '12345', 1000.00);
-
-INSERT INTO conta_corrente (nome_do_titular, numero_da_conta, saldo_atual)
-VALUES ('Maria Oliveira', '56789', 2500.50);
